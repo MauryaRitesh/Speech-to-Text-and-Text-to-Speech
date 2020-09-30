a7768bfbd4ee8b831f423e5221d5bc5856f77627
PranjalKhatri <68675142+PranjalKhatri@users.noreply.github.com>
2020-09-30 21:15:28 +0530

    Update speak.py
    Added comments

--- speak.py
+++ speak.py
@@ -2,7 +2,10 @@ from gtts import gTTS
 import pyglet
 import time, os
 
+#Speak function tts = text to speech
 def tts(text, lang):
+    #text : The text you want to be spoken
+    #lang : the language you want the text to be spoken
     file = gTTS(text = text, lang = lang)
     filename = '/tmp/temp.mp3'
     file.save(filename)
